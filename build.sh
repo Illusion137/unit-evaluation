c24ad8250446681eb5300f3aabd3782050e84f2f
Illusion137 <raygoza.d@hotmail.com>
2026-02-24 14:12:23 -0700

    chore: migrate as Nero from UnitEval

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 # emcmake cmake -B build-wasm -S .
 emcmake cmake -S . -B build-wasm
 cmake --build build-wasm
-rm /Users/illusion/dev/Everett/public/wasm/UnitEval.js
-rm /Users/illusion/dev/Everett/public/wasm/UnitEval.wasm
+rm /Users/illusion/dev/Everett/public/wasm/Nero.js
+rm /Users/illusion/dev/Everett/public/wasm/Nero.wasm
 rm /Users/illusion/dev/Everett/src/dimension_wasm_interface.ts
-rm -f /Users/illusion/dev/Everett/src/UnitEval.d.ts
-cp build-wasm/UnitEval.js /Users/illusion/dev/Everett/public/wasm/UnitEval.js
-cp build-wasm/UnitEval.wasm /Users/illusion/dev/Everett/public/wasm/UnitEval.wasm
-cp build-wasm/UnitEval.d.ts /Users/illusion/dev/Everett/src/UnitEval.d.ts
+rm -f /Users/illusion/dev/Everett/src/Nero.d.ts
+cp build-wasm/Nero.js /Users/illusion/dev/Everett/public/wasm/Nero.js
+cp build-wasm/Nero.wasm /Users/illusion/dev/Everett/public/wasm/Nero.wasm
+cp build-wasm/Nero.d.ts /Users/illusion/dev/Everett/src/Nero.d.ts
 cp dimension_wasm_interface.ts /Users/illusion/dev/Everett/src/dimension_wasm_interface.ts
